0c1651ba686e2e9bd41cda15b124ee388dc23ea2
Kayla Lee <kel7@sfu.ca>
2022-01-15 14:09:20 -0800

    Added the JDA bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception
+    {
+        JDA api = JDABuilder.createDefault(" "
+        ).build();
     }
 }
+
+
