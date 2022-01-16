60501d364512e7c019a4438424bec3f187ed31ce
Brendan Saw <brendansaw@gmail.com>
2022-01-15 19:36:38 -0800

    Added some comments; minor bug fixing

--- src/main/java/EventScheduler.java
+++ src/main/java/EventScheduler.java
@@ -6,76 +6,79 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import java.util.*;
 import java.util.HashMap;
 
-
-/*
 public class EventScheduler extends ListenerAdapter {
-    public static void main(String[] args){
-        //Creating Hash Map
-        HashMap<String, String> map = new HashMap<>();
-
-        //Getting user input
-        String UserInput = event.getMessage();
-        String content = message.getContentRaw();
-
-    }
-}
-*/
-
-public class EventScheduler extends ListenerAdapter
-{
     private int key = 1;
-    HashMap<String, String> map1 = new HashMap<>();
-    HashMap<String, ArrayList<String>> map2 = new HashMap<>();
-    HashMap<String, String> map3 = new HashMap<>();
+
+    /**
+     * (Command key, command value) pair
+     * HashMap<String, Pair<String, MessageChannel>>
+     * {
+     *
+     *     "1": a
+     *
+     * }
+     * Where a.getKey() is equal to "@Derick shut up"
+     * and a.getValue() is equal to the MessageChannel from where the command was received from
+     */
+    HashMap<String, String> hashKeyToMessage = new HashMap<>();
+
+    /**
+     * (Time, list of command keys) pair
+     * HashMap<String, List<String>>
+     * {
+     *
+     *     "2:00": {"1"}
+     *
+     *
+     * }
+     */
+    HashMap<String, ArrayList<String>> hashTimeToKeys = new HashMap<>();
+
+    /**
+     * (Command key, time) pair
+     * HashMap<String, String>
+     * {
+     *
+     *     "1": "2:00"
+     *
+     * }
+     *
+     */
+    HashMap<String, String> hashKeyToTime = new HashMap<>();
+
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event)
-    {
+    public void onMessageReceived(MessageReceivedEvent event) {
         //hashmap1
         Message message = event.getMessage();
         String content = message.getContentRaw();
 
 
-        String[] contentString = content.split(" ",2);
+        String[] contentString = content.split(" ",3);
+        // split string into ["!schedule", "[time]", "[message]"]
 
-        ArrayList<String> contentArrList = new ArrayList<String>();
-        Collections.addAll(contentArrList,contentString);
+        ArrayList<String> contentArrList = new ArrayList<>();
+        Collections.addAll(contentArrList, contentString);
 
-
-        System.out.println(map1);
-        System.out.println(contentArrList.size());
-
-        if (contentArrList.get(0).equals("!schedule")) //check
-        {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
-        } else if (content.equals("!bingchilling")) {
-            MessageChannel channel = event.getChannel();
-
-        }
         String strKey = String.valueOf(key);
-        map1.put(strKey, content);
 
+        hashKeyToMessage.put(strKey, content);
 
         //hashmap2
         String time = contentArrList.get(1);
-        ArrayList<String> arrKeys = new ArrayList<String>();
+        ArrayList<String> arrKeys = new ArrayList<>();
 
-        if (map2.containsKey(time)) {
-            map2.get(time).add(strKey);
+        if (hashTimeToKeys.containsKey(time)) {
+            hashTimeToKeys.get(time).add(strKey);
         } else {
             arrKeys.add(strKey);
-            map2.put(time, arrKeys);
+            hashTimeToKeys.put(time, arrKeys);
         }
 
-
-
         //hashmap3
-
-        map3.put(strKey, time);
+        hashKeyToTime.put(strKey, time);
 
         key++;
-
 
     }
 }
