83fc0a6e8fce8fa80b3ca3a8195bb32fa5aba238
Kayla Lee <kel7@sfu.ca>
2022-01-16 00:33:48 -0800

    delete event

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
                 .addEventListeners(new BotListener())
                 .addEventListeners(new EventScheduler())
                 .addEventListeners(new InfoListener())
+                .addEventListeners(new DeleteEvent())
                 .build();
 
         Timer timer = new Timer();
